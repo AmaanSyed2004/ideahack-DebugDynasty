e1e805d610e93c100555a507e54367dae351f5ee
Pratham2994 <prathampanchal02994@gmail.com>
2025-03-16 14:10:09 +0530

    added homeuserpage

---
+++ client/src/components/MyQueries.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyQueries = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-blue-50">
+            <h1 className="text-3xl font-bold text-gradient">My Queries (Coming Soon!)</h1>
+        </div>
+    )
+}
+
+export default MyQueries
