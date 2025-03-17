f12a0e8a30633f32a78f52c1595bcd086397ef15
AmaanSyed2004 <amaansyed2004@gmail.com>
2025-03-18 04:53:20 +0530

    add routes

---
+++ server/controllers/appointment/getAppointmentUser.js
@@ -0,0 +1,20 @@
+const Appointment= require("../../models/Appointment");
+const getAppointmentUser = async (req, res) => {
+    const userID = req.user.id;
+    if (!userID) {
+        return res.status(400).json({ message: "Please provide user ID" });
+    }
+    const appointments = await Appointment.findAll({
+        where: { customerID:userID },
+    });
+    if (!appointments || appointments.length === 0) {
+        return res.status(404).json({
+            message: "No appointments found",
+        });
+    }
+    return res.status(200).json({
+        appointments
+    });
+};
+
+module.exports = getAppointmentUser;
--- server/routes/appointment/appointmentRouter.js
+++ server/routes/appointment/appointmentRouter.js
@@ -1,11 +1,14 @@
 const express= require('express');
 const getAvailableSlots = require('../../controllers/appointment/getAvailableSlots');
-const { authenticateJWT, authenticateCustomer } = require('../../middleware/authMiddleware');
+const { authenticateJWT, authenticateCustomer, authenticateWorker } = require('../../middleware/authMiddleware');
 const bookAppointment = require('../../controllers/appointment/book');
+const getAppointmentUser = require('../../controllers/appointment/getAppointmentUser');
+const getAppointmentWorker = require('../../controllers/appointment/getAppointmentWorker');
 
 const appointmentRouter= express.Router();
 
 appointmentRouter.get('/slots', authenticateJWT, getAvailableSlots);
 appointmentRouter.post('/book', authenticateJWT, authenticateCustomer, bookAppointment)
-
+appointmentRouter.get('/customer/', authenticateJWT, authenticateCustomer, getAppointmentUser)
+appointmentRouter.get('/worker/', authenticateJWT, authenticateWorker, getAppointmentWorker)
 module.exports= appointmentRouter;
